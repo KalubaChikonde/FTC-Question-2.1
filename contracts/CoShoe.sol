777de53e8457175be09db64ab99023b9560daf37
Kaluba Chikonde <kaluba.c96@gmail.com>
2019-08-02 21:04:42 +0200

    Complete CoShoe contract

--- contracts/CoShoe.sol
+++ contracts/CoShoe.sol
@@ -14,8 +14,30 @@ contract CoShoe {
     Shoe[] public shoes;
     
     constructor() public{
-        shoes.push(Shoe(msg.sender,"","",false)); 
+        //mint 100 token 
+        shoes.push(Shoe(msg.sender,"","",false)) -1 ; 
     }
 
+    function buyShoe(string memory name,string memory image) public payable {
 
+        require(shoes.length > 0 ,''); //check that there is still pair of shoes left has not been sold
+        require(msg.value == price, 'Price is not sufficient or too much'); // 
+        shoes.push(Shoe(msg.sender, name,image,true));
+        ShoesSold++;
+
+    }
+
+    function checkPurchases() external view returns(bool[] memory) {
+        bool[] memory checkBuy; 
+            // loop through the buyer addresses
+        for (uint i = 0; i < shoes.length; i++){
+        // check whether the msg.sender is among the buyer addresses
+        if (shoes[i].owner == msg.sender){
+            // if yes, return true
+            checkBuy[i] = true;
+        }
+        }
+        return checkBuy;
+
+  }
 }
